c283ee4fbf911ea68fbedabc0b5c0a6258dac868
Gloria <gloriaesthermartinezmartinez@gmail.com>
2026-01-08 00:40:12 -0600

    STYLE:Mejoras en el estilo del menu

--- crm/static/js/hamburger.js
+++ crm/static/js/hamburger.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   hamburger?.addEventListener("click", () => {
     menu.classList.toggle("active");
     overlay.classList.toggle("active");
+    hamburger.classList.toggle("active");
   });
 
   overlay?.addEventListener("click", () => {
     menu.classList.remove("active");
     overlay.classList.remove("active");
+    hamburger.classList.remove("active");
   });
 });
