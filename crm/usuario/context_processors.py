8d560fc3dbdf7f04bfb48283933b9f8f197e0ff5
Gloria <gloriaesthermartinezmartinez@gmail.com>
2026-01-12 01:53:35 -0600

    FEAT:Permitir que empleados vean el logo del negocio

--- crm/usuario/context_processors.py
+++ crm/usuario/context_processors.py
@@ -1,7 +1,9 @@
 from usuario.models import Usuario
 from usuario.models import PreferenciaUsuario
 
-def usuario_actual(request):
+def usuario_actual(request):#te quedaste en resolver lo del logo, lo demas funciona, lo unico que debes haceres
+    #encargarte de terminar la documentacion y la liberacion de residencia a tiempo
+    #ademas tienes junta a las 10 
     usuario = None
     preferencias = None
 
@@ -13,11 +15,17 @@ def usuario_actual(request):
         ).select_related("rol").first()
 
         if usuario:
+            dueno = (
+                usuario
+                if usuario.rol.nombre_rol == "Dueño"
+                else usuario.owner_id
+            )
             preferencias = PreferenciaUsuario.objects.filter(
-                usuario=usuario
+                usuario=dueno
             ).first()
 
     return {
         "usuario": usuario,
         "preferencias": preferencias
     }
+
