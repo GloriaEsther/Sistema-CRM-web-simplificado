efda26a27c55bf903ac22caaa72ed97c83e77b35
Gloria <gloriaesthermartinezmartinez@gmail.com>
2026-05-11 23:20:27 -0600

    Refactor: Configurar variables de entorno

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,11 +88,11 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'crm_db',
-        'USER': 'user',           
-        'PASSWORD': 'yourpassword',       
-        'HOST': '127.0.0.1',
-        'PORT': 'XXXX',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),           
+        'PASSWORD': os.getenv('DB_PASSWORD'),       
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
